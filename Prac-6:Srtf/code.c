18717084fb5960614bbc9f4a30bcc529a54ddafa
Anjali <91792558+anjalicpp@users.noreply.github.com>
2022-10-12 17:18:56 +0530

    Create code.c

---
+++ Prac-6:Srtf/code.c
@@ -0,0 +1,6 @@
+//srtf scheduling 
+SHORTEST-JOB-FIRSTSCHEDULING (SJFS):
+This Algorithm is the preemptive version of SJF scheduling. In SRTF, the execution of the process can be stopped after certain amount of time. 
+At the arrival of every process, the short term scheduler schedules the process with the least remaining burst time among the list of available processes
+and the running process
+CODE:
